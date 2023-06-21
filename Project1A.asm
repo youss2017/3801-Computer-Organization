3dfec681fd64fc30e5b65c9d95a8f08600c62c86
youss2017 <samwelyoussef123.2@gmail.com>
2023-06-21 12:40:20 -0400

    6/21/2023

--- Project1A.asm
+++ Project1A.asm
@@ -1,151 +1,152 @@
+# Register used
+# t0-t4 used for loop counters and multipcation
+# a0 used for arguments
+# v0 syscall id
 
 .text
 start:
 	# Prompt User to enter input
-	la $a0, Prompt
-	li $v0, 4
+	la $a0, Prompt # load prompt text address
+	li $v0, 4 # load print syscall id
 	syscall
+	jal print_new_line # print new line
 	# Read User Input; read into memory (stack)
-	li $v0, 5
+	li $v0, 5 # read integer
 	syscall
-	sw $v0, A
+	sw $v0, A # store integer value into A
 	
-	li $v0, 5
-	syscall
-	sw $v0, B
+	jal print_new_line # print new line
 	
-	li $v0, 5
+	li $v0, 5 # read integer
 	syscall
-	sw $v0, C
+	sw $v0, B # store integer value into B
 	
-	li $v0, 5
+	jal print_new_line
+	
+	li $v0, 5 # read integer
 	syscall
-	sw $v0, D
+	sw $v0, C # store integer value into C
+	
+	jal print_new_line
+	
+	li $v0, 5 # read integer
+	syscall
+	sw $v0, D # store integer value into D
+	
+	jal print_new_line
 	
 	# compute f=5 * B * D + A
-	li $t0, 5
-	lw $t1, B
+	li $t0, 5 # load 5 into $t0
+	lw $t1, B # load B into $t1
 	# t2 = 5 * B
 
 	# init t2
-	li $t2, 0
+	li $t2, 0 # reset loop counter
 	loop_start1:
-	add $t2, $t2, $t1
-	addi $t0, $t0, -1
-	bgtz $t0, loop_start1
+	add $t2, $t2, $t1 # perform multiply add
+	addi $t0, $t0, -1 # decrement loop counter by 1
+	bgtz $t0, loop_start1 # $t0 > 0
 	
-	move $t0, $t2
-	lw $t1, D
+	move $t0, $t2 # move multiplication result into $t0
+	lw $t1, D # read value at address (D) into $t1
 	# t2 = 5 * B * D
 
 	# init t2
-	li $t2, 0
+	li $t2, 0 # reset counter
 	loop_start2:
-	add $t2, $t2, $t1
-	addi $t0, $t0, -1
-	bgtz $t0, loop_start2
-	
-	lw $t0, A
-	add $t2, $t2, $t0
+	add $t2, $t2, $t1 # perform multiply add
+	addi $t0, $t0, -1 #  decrement loop counter by 1
+	bgtz $t0, loop_start2 # $t0 > 0
+	lw $t0, A # Read value at address A into $t0
+	add $t2, $t2, $t0 # Add to value of A to t2 register to create f
 	
 	# print f result
 	# print F_ten
-	la $a0, F_Ten
-	li $v0, 4
+	la $a0, F_Ten # Load memory address of F_ten
+	li $v0, 4 # Load print string syscall id
 	syscall
-	
-	li $v0, 1
-	move $a0, $t2
+		
+	li $v0, 1 # load print integer syscall
+	move $a0, $t2 # set a0 to t2
 	syscall
-	
-	# new line
+
 	jal print_new_line
-	
+	jal print_new_line
+
 	# print result in binary
 	# print F_two
-	la $a0, F_Two
-	li $v0, 4
+	la $a0, F_Two # Load F_two string address into register
+	li $v0, 4 # Load print string syscall
 	syscall
 	
-	li $v0, 35
-	move $a0, $t2
+	li $v0, 35 # load print integer as binary syscall id
+	move $a0, $t2 # set argument value to t2
 	syscall
+	
+	jal print_new_line
 	
 	# compute g=D*D - C*A
-	lw $t0, D
-	lw $t1, D
+	lw $t0, D # Load value of D from memory
+	lw $t1, D # Load value of D from memory
 	#compute D*D into t2
 
 	# init t2
-	li $t2, 0
+	li $t2, 0 # reset counter
 	loop_start3:
-	add $t2, $t2, $t1
-	addi $t0, $t0, -1
-	bgtz $t0, loop_start3
+	add $t2, $t2, $t1 # multiplication add
+	addi $t0, $t0, -1 # decrement loop counter
+	bgtz $t0, loop_start3 # if greater than 0
 	
 	# set t4 to D*D
-	move $t4, $t2
-	lw $t0, C
-	lw $t1, A
+	move $t4, $t2 # Save multiplication value to t4
+	lw $t0, C # load value of C
+	lw $t1, A # load value of A
 	# compute C*A
 
 	# init t2
-	li $t2, 0
+	li $t2, 0 # reset counter
 	loop_start4:
-	add $t2, $t2, $t1
-	addi $t0, $t0, -1
-	bgtz $t0, loop_start4
+	add $t2, $t2, $t1 # Multication addition
+	addi $t0, $t0, -1 # decerement loop counter
+	bgtz $t0, loop_start4 # jump if > 0
 	
 	# compute D*D - C*A ==> t0 = t4 - t2
 	sub $t0, $t4, $t2
 	
-	# new line
 	jal print_new_line
-	
+
 	# print G_ten
-	la $a0, G_Ten
-	li $v0, 4
+	la $a0, G_Ten # load g_ten string address
+	li $v0, 4 # load print address
 	syscall
 	
 	#print g
-	move $a0, $t0
-	li $v0, 1
+	move $a0, $t0 # load integer value
+	li $v0, 1 # print integer syscall id
 	syscall
 	
-	# new line
+	jal print_new_line
 	jal print_new_line
 	
 	# print G_two
-	la $a0, G_Two
-	li $v0, 4
+	la $a0, G_Two # load G_two string address
+	li $v0, 4 # print string syscall id
 	syscall
 	
 	# print in binary
-	move $a0, $t0
-	li $v0, 35
+	move $a0, $t0 # load g result into a0
+	li $v0, 35 # print integer in binary form
 	syscall
-		
+	
+	jal print_new_line
+	jal print_new_line
+				
 	#exit
 	li $v0, 10
 	syscall
-
-# multiples value in $t0 with $t1 and outputs in $t2
-loop_mul:
-	# init t2
-	li $t2, 0
-	loop_start:
-	add $t2, $t2, $t1
-	addi $t0, $t0, -1
-	bgtz $t0, loop_start
-	jr $ra
-	
-print_new_line:
-	li $v0, 11
-	li $a0, 10
-	syscall
-	jr $ra
 	
 .data:
+# Variables to store user input and string constants
 	A: .word 0
 	B: .word 0
 	C: .word 0
---
+++ Project1B.asm
@@ -0,0 +1,163 @@
+# Youssef Samwel 6/21/2023
+# s0 is f_ten
+# s1 is g_ten
+# v0 syscall arguments; syscall id
+# a0 syscall arguments
+# s0, s1 used to compute f_ten
+# s3, s4; compute division and remainder/mod
+# t0, t1 compute g_ten
+# t0 t1 t2 compute division/remainder; t1 used for loop counter
+
+.text
+start:
+	############### Read user input
+	
+	li $v0, 4 # load print string syscall id
+	la $a0, f_ten_str # load string address
+	syscall
+	
+	li $v0, 5 # load read integer syscall id
+	syscall
+	move $s0, $v0 # temporarily store value in v0 to s0 (f_ten)
+	sw $v0, f_ten_var
+	
+	jal print_new_line
+	
+	li $v0, 4 # load print string syscall id
+	la $a0, g_ten_str # load string address
+	syscall
+	
+	li $v0, 5 # load read integer syscall id
+	syscall
+	move $s1, $v0 # temporarily store value in v0 to s1 (g_ten)
+	
+	# Compute f_ten + 2
+	addi $s0, $s0, 2
+	# Compute g_ten - 2
+	addi $s1, $s1, -2
+	
+	# branch if g_ten - 2 <= 0
+	ble $s1, 0, error_handler
+	
+	###################### Compute (f_ten + 2) / (g_ten - 2)
+	# store quotient in s3
+	# store remainder in s4
+	div_loop:
+	# substract s1 from s0; s0 - s1
+	sub $s0, $s0, $s1
+	addi $s3, $s3, 1
+	# if s0 > 0 then add 1 to s3 and jump to div_start
+	# otherwise add s1 to s0; s0 + s1
+	# and set s4 to s1 (which is the remainder)
+	bgt $s0, 0, div_loop
+	# jump to print output if s0==0
+	beqz $s0, print_output
+	
+	############## Compute remainder
+	add $s0, $s0, $s1 # s0 += s1
+	subi $s3, $s3, 1 # s3 += 1
+	move $s4, $s0 # s4=s0
+	
+	################# Display results (part 1)
+	print_output:
+	jal print_new_line
+	
+	li $v0, 4 # print syscall id
+	la $a0, h_quotient_str # load string address
+	syscall
+	
+	li $v0, 1 # print integer syscall id
+	move $a0, $s3 # set a0 to s3
+	syscall
+	
+	jal print_new_line # jump to print new line
+	jal print_new_line # jump to print new line
+	
+	li $v0, 4 # load print id syscall id
+	la $a0, h_remainder_str # load string address
+	syscall
+	
+	li $v0, 1
+	move $a0, $s4
+	syscall
+	
+	################### Compute division to determine remainder
+	# store quotient in t1
+	# store remainder in t2
+	# if h_remainder or s4 is zero then jump to print_output2 because mod is 0
+	# compute (f_ten-25) into t0
+	lw $t0, f_ten_var
+	addi $t0, $t0, -25
+	
+	li $t2, 0 # t2 = 0 ; loop counter
+	beqz $s4, print_output2 # s4 != 0
+	div_loop2:
+	# substract t0 from s4; t0 - s4
+	sub $t0, $t0, $s4
+	addi $t1, $t1, 1
+	# if t0 > 0 then add 1 to t1 and jump to div_start
+	# otherwise add s4 to t0; t0 + s4
+	# and set t2 to s0 (which is the remainder)
+	bgt $t0, 0, div_loop2
+	# jump to print output if s0==0
+	beqz $t0, print_output2
+	
+	# compute remainder
+	add $t0, $t0, $s4 # t0+=s4
+	subi $t1, $t1, 1 # t1+=1
+	move $t2, $t0 # t2=t0
+	
+	print_output2:
+	jal print_new_line  # jump to print new line
+	jal print_new_line  # jump to print new line
+	
+	li $v0, 4 # load print string syscall id
+	la $a0, i_mod_str # load string address
+	syscall
+	
+	li $v0, 1 # load print int syscall id
+	move $a0, $t2 # setargument
+	syscall	# print number
+	
+	jal print_new_line # jump to routuine 
+	
+	jal print_new_line  # jump to print new line
+	j start # return to start of program
+	
+	# exit
+	li $v0, 10 # load exit syscall id
+	syscall # exit
+	
+print_new_line:
+	li $v0, 11 # print character syscall id
+	li $a0, 10 # ascii '\n'
+	syscall
+	jr $ra
+	
+error_handler:
+	jal print_new_line # jump to print new line
+	li $v0, 4 # load print string syscall id
+	la $a0, error_str # load string address
+	syscall
+	jal print_new_line  # jump to print new line
+	# ask users again for inputs
+	j start # return to start of program
+	
+.data
+	# strings
+	f_ten_str: .asciiz "f_ten = "
+	g_ten_str: .asciiz "g_ten = "
+	error_str: .asciiz "g_ten must be greater than 2 (e.g. g_ten - 2 > 0)\n"
+	h_quotient_str: .asciiz "h_quotient = "
+	h_remainder_str: .asciiz "h_remainder = "
+	i_mod_str: .asciiz "i_mod = "
+	# use variable to reduce complex logic required
+	# it would be more complicated to only store f_ten in
+	# registers because the program flow would be more
+	# convoluted.
+	f_ten_var: .word 0
+	
+	
+	
+	
+	
